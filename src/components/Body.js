c578570143785c2d7bef0cdb557d32d6c184bf61
sivagopi <pallagopi00@gmail.com>
2026-01-09 01:08:59 +0530

    RestaurantCard Component

--- src/components/Body.js
+++ src/components/Body.js
@@ -14,14 +14,6 @@ const Body = () => {
           <h4>deliverytime</h4>
           <h4>cost for two</h4>
         </div>
-        <div className="res-card">
-          <img src="null" alt="resimage" />
-          <h4>ResName</h4>
-          <h4>Rating</h4>
-          <h4>cuisines</h4>
-          <h4>deliverytime</h4>
-          <h4>cost for two</h4>
-        </div>
       </div>
     </div>
   );
