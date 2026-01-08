7c772164785b8e8aefd10392ba1dd77dcf7d40d5
sivagopi <pallagopi00@gmail.com>
2026-01-08 20:26:16 +0530

    first rendered app

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
+import ReactDOM from "react-dom/client";
 const AppLayout = () => {
   return <div className="App"></div>;
 };
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
